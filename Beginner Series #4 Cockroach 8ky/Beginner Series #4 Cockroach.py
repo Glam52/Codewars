10fe99ed37b9dfb92bf291be91fed571956c1dfe
Glam52 <gla_max@outlook.com>
2023-12-17 17:29:11 +0700

    Добавлены комментарии

--- Beginner Series #4 Cockroach 8ky/Beginner Series #4 Cockroach.py
+++ Beginner Series #4 Cockroach 8ky/Beginner Series #4 Cockroach.py
@@ -1,4 +1,5 @@
-def cockroach_speed(s):
-    return int(s * 100000 / 3600)
+def cockroach_speed(s):         # созд. функции по условию
+    return int(s * 100000 / 3600)           # возвращает целое, по формуле
 
+#тест, в задание не входит
 print(cockroach_speed(1.08))
