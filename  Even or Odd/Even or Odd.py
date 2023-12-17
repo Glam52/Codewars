cc7c9c05356edfd2818e1d701abc460f2fdc6cca
Glam52 <gla_max@outlook.com>
2023-12-17 16:53:07 +0700

    Написание комментариев

---  Even or Odd/Even or Odd.py
+++  Even or Odd/Even or Odd.py
@@ -1,9 +1,10 @@
 def even_or_odd(number):            # функция по условию
-    if number % 2 == 0:
+    if number % 2 == 0:         # остаток при делении на 2
         return 'Even'
     else:
         return 'Odd'
     pass            # заполнитель по условию
 
+#проверка, в заданиен не входит
 n = int(input())
 print(even_or_odd(n))
