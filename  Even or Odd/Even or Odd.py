8ce14e015de394436bb86a9c066e4451ae58ebbd
Glam52 <gla_max@outlook.com>
2023-12-17 16:50:10 +0700

    Готовый код + проверка

---  Even or Odd/Even or Odd.py
+++  Even or Odd/Even or Odd.py
@@ -1,3 +1,9 @@
 def even_or_odd(number):            # функция по условию
-
+    if number % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
     pass            # заполнитель по условию
+
+n = int(input())
+print(even_or_odd(n))
