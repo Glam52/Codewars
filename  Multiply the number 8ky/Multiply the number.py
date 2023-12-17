0432abcc8fbc4631f662ad275e4f70824f24cc4d
Glam52 <gla_max@outlook.com>
2023-12-17 17:57:22 +0700

    Добавил проверку на отрицательное число

---  Multiply the number 8ky/Multiply the number.py
+++  Multiply the number 8ky/Multiply the number.py
@@ -1,6 +1,10 @@
 def multiply(n):            # создана функция по условию
-    x = len(str(n))          # узнаем длину числа
+    if n >= 0:
+        x = len(str(n))          # узнаем длину числа
+    else:
+        x = len(str(n)) - 1         # проверка на отрицательное число
     return n * 5 ** x           # умножаем n на 5 в степени x
     pass            # заполнитель по условию
 
 print(multiply(10))
+print(multiply(-3))
