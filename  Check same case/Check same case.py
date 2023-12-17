c823a6fa50ef6125a9d5d2788448f648c14a75f2
Glam52 <gla_max@outlook.com>
2023-12-17 16:09:18 +0700

    init

---
+++  Check same case/Check same case.py
@@ -0,0 +1,10 @@
+def same_case(a, b):
+    if not a.isalpha() or not b.isalpha():
+        return  -1  #если не буквы вернуть -1
+    elif a.islower() and b.islower():
+        return 1
+    elif a.isupper() and b.isupper():
+        return 1
+    else:
+        return 0
+    pass
