d0f09691d44a424636261ee93f85d97e5cd5a119
Glam52 <gla_max@outlook.com>
2023-12-17 16:15:54 +0700

    Добавлены коментарии ко всем строкам

---  Check same case/Check same case.py
+++  Check same case/Check same case.py
@@ -1,10 +1,10 @@
 def same_case(a, b):
     if not a.isalpha() or not b.isalpha():
-        return  -1  #если не буквы вернуть -1
+        return  -1          # если не буквы вернуть -1
     elif a.islower() and b.islower():
-        return 1
+        return 1            # проверка нижнего регистра
     elif a.isupper() and b.isupper():
-        return 1
+        return 1            # проверка верхнего регистра
     else:
-        return 0
+        return 0            # остальное
     pass # новый комментарий
