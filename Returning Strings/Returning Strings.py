7835882c2f1e9bd1726c70095a1df35024456f28
Glam52 <gla_max@outlook.com>
2023-12-19 13:50:44 +0700

    Смена print на return (по условию функция должна "вернуть" значение)

--- Returning Strings/Returning Strings.py
+++ Returning Strings/Returning Strings.py
@@ -1,6 +1,7 @@
 def greet(name):        # добавлена функция по условию
-    print(f"\'Hello, {name} how are you doing today?\'")        # f строка с переменной name
+    return (f"\'Hello, {name} how are you doing today?\'")        # f строка с переменной name
     pass
 
 # тест, не входит в задание
-greet('Max')
+print(greet('Max'))
+
