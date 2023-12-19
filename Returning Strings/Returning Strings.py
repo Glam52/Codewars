b2d2c7e9ad3357779d84ac8bd79c0d9e852eda7d
Glam52 <gla_max@outlook.com>
2023-12-19 13:41:06 +0700

    добавил кавычки в тексте, но тест не пропускает

--- Returning Strings/Returning Strings.py
+++ Returning Strings/Returning Strings.py
@@ -1,5 +1,5 @@
 def greet(name):        # добавлена функция по условию
-    print(f"Hello, {name} how are you doing today?")        # f строка с переменной name
+    print(f"\'Hello, {name} how are you doing today?\'")        # f строка с переменной name
     pass
 
 # тест, не входит в задание
