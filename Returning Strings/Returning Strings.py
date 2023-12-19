6f9becd289c22e406db628d59755117f4e83695d
Glam52 <gla_max@outlook.com>
2023-12-19 13:36:05 +0700

    доработан комментарий

--- Returning Strings/Returning Strings.py
+++ Returning Strings/Returning Strings.py
@@ -2,5 +2,5 @@ def greet(name):        # добавлена функция по условию
     print(f"Hello, {name} how are you doing today?")        # f строка с переменной name
     pass
 
-####
+# тест, не входит в задание
 greet('Max')
