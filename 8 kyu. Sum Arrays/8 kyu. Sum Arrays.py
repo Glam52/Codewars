f6318cd3c8fb9ca4bd99f71f3f098cbc8adccf77
Glam52 <gla_max@outlook.com>
2023-12-20 13:58:17 +0700

    тестирование

--- 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
+++ 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
@@ -7,3 +7,7 @@ def sum_array(a):
 
 # тест
 print(sum_array([1, 5.2, 4, 0, -1]))
+print(sum_array([1, 2, 3]))
+print(sum_array([1.1, 2.2, 3.3]))
+print(sum_array([4, 5, 6]))
+print(sum_array(range(101)))
