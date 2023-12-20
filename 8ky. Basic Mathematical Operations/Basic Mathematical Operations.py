ada118f8ed3aa55c01b87b66be8aca701bf41106
Glam52 <gla_max@outlook.com>
2023-12-20 13:16:47 +0700

    Подготовка функции

--- 8ky. Basic Mathematical Operations/Basic Mathematical Operations.py
+++ 8ky. Basic Mathematical Operations/Basic Mathematical Operations.py
@@ -1,1 +1,9 @@
 def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2      # сложение
+    elif operator == '-':
+        return value1 - value2      # вычитание
+    elif operator == '*':
+        return value1 * value2      # умножение
+    elif operator == '/':
+        return value1 / value2      # деление
