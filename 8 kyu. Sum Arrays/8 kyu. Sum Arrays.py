2c3d8571e16ddc2c42e9167bb7c527eba113ae94
Glam52 <gla_max@outlook.com>
2023-12-20 13:53:44 +0700

    Переименование count в amount

--- 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
+++ 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
@@ -1,9 +1,9 @@
 def sum_array(a):
-    x = list(map(float, a))
-    count = 0
+    x = list(map(float, a))     # перевод значений списка в float
+    amount = 0       # Lj,fdkty
     for i in x:
-        count += i
-    return count
+        amount += i
+    return amount
 
 print(sum_array([1, 5.2, 4, 0, -1]))
 
