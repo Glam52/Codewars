3440cec87a1f4df1acafa58e9048cf6ebdf367f6
Glam52 <gla_max@outlook.com>
2023-12-20 13:55:10 +0700

    Добавление комментариев

--- 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
+++ 8 kyu. Sum Arrays/8 kyu. Sum Arrays.py
@@ -1,9 +1,9 @@
 def sum_array(a):
-    x = list(map(float, a))     # перевод значений списка в float
-    amount = 0       # Lj,fdkty
-    for i in x:
-        amount += i
+    x = list(map(float, a))  # перевод значений списка в float
+    amount = 0  # сумма списка
+    for i in x:  # цикл перебирающий значения списка
+        amount += i  # суммирование значений списка
     return amount
 
+# тест
 print(sum_array([1, 5.2, 4, 0, -1]))
-
