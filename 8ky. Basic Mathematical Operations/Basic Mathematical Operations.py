941b3fce5b2d3d8b0291ffc11a15e717f7889722
Glam52 <gla_max@outlook.com>
2023-12-20 13:18:31 +0700

    Тестирование функции

--- 8ky. Basic Mathematical Operations/Basic Mathematical Operations.py
+++ 8ky. Basic Mathematical Operations/Basic Mathematical Operations.py
@@ -7,3 +7,9 @@ def basic_op(operator, value1, value2):
         return value1 * value2      # умножение
     elif operator == '/':
         return value1 / value2      # деление
+
+# тест
+print(basic_op('+', 4, 7))
+print(basic_op('-', 15, 18))
+print(basic_op('*', 5, 5))
+print(basic_op('/', 49, 7))
