c5f416761afeceae3ba8f7a9010b8a273cc50ae6
Glam52 <gla_max@outlook.com>
2023-12-20 14:23:57 +0700

    Добавлены комментарии

--- 7 kyu/7 kyu. Disemvowel Trolls/Disemvowel Trolls.py
+++ 7 kyu/7 kyu. Disemvowel Trolls/Disemvowel Trolls.py
@@ -1,9 +1,10 @@
 def disemvowel(string_):
-    string_ = list(string_)
-    for i in string_[::-1]:
-        if i in 'eioauEIOAU':
-            string_.remove(i)
+    string_ = list(string_)     # перевод строки в список
+    for i in string_[::-1]:     # перебор списка посимвольно с конца
+        if i in 'eioauEIOAU':       # проверка на гласные
+            string_.remove(i)       # удаление гласных
 
-    return str(''.join(string_))
+    return str(''.join(string_))        # возврат строки
 
+# тест
 print(disemvowel('This website is for losers LOL!'))
